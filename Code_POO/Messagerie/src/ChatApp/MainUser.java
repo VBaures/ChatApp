3b57bbe4d9912543a44eda17baa13102297e88d2
VBaures <bauresvincent@outlook.com>
2020-11-23 16:45:09 +0100

    User and MainUser

--- Code_POO/Messagerie/src/ChatApp/MainUser.java
+++ Code_POO/Messagerie/src/ChatApp/MainUser.java
@@ -2,4 +2,7 @@ package ChatApp;
 
 public class MainUser extends User {
 
+		void setPseudo (String pseudo) {
+			super.pseudo=pseudo;
+		}
 }
--- Code_POO/Messagerie/src/ChatApp/User.java
+++ Code_POO/Messagerie/src/ChatApp/User.java
@@ -1,5 +1,23 @@
 package ChatApp;
 
 public class User {
-
+	private String username;
+	private String password;
+	protected String pseudo;
+	private int ID;
+	
+	String getUserName () {
+		return this.username;
+	}
+	
+	String getPassword () {
+		return this.password;
+	}
+	
+	String getPseudo () {
+		return this.pseudo;
+	}
+	int getID () {
+		 return this.ID;
+	}
 }
