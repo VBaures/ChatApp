bc1754ac96d09e0a501aa845cde9686d5731d1cd
AliciaCalmet <acalmet@etud.insa-toulouse.fr>
2020-11-25 08:32:30 +0100

    changement

--- Code_POO/src/ChatApp/Message.java
+++ Code_POO/src/ChatApp/Message.java
@@ -1,9 +1,9 @@
 package ChatApp;
-
+import java.util.Date;
 public class Message {
     protected Date time;
     protected User recipient;
-    public Date getTime (){this.time;}
+    public Date getTime (){return this.time;}
 
     public User getRecipient() {
         return this.recipient;
