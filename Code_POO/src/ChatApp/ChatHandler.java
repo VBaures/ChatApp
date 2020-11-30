197b7b0418630fdd069f01d6b4fc472528f11d7a
VBaures <bauresvincent@outlook.com>
2020-11-30 17:57:47 +0100

    Reset

--- Code_POO/src/ChatApp/ChatHandler.java
+++
@@ -1,4 +0,0 @@
-package ChatApp;
-
-public class ChatHandler {
-}
--- Code_POO/src/ChatApp/FileMessage.java
+++
@@ -1,7 +0,0 @@
-package ChatApp;
-import java.io.File;
-public class FileMessage extends Message {
-    protected File Content;
-    public File getContent (){return this.Content ;}
-    public void setContent (File fichier) {}
-}
