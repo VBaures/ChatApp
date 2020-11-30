3bd1045b741a53859869137ecba9ba0bbd9383a8
AliciaCalmet <acalmet@etud.insa-toulouse.fr>
2020-11-30 17:45:01 +0100

    Revert "Merge branch 'master' of https://github.com/VBaures/Projet-COO"
    This reverts commit 086469287aa93954a77ca21fbcc86a78e9f7b9ca, reversing
    changes made to 9baf20bf417e200f92e82f1a946039a6eed5b45e.

--- Code_POO/src/ChatApp/PseudoHandler.java
+++ Code_POO/src/ChatApp/PseudoHandler.java
@@ -44,9 +44,5 @@ public class PseudoHandler {
             main_User.pseudo = Pseudo;
         }
     }
-    public boolean IsConnectedUser (User user) {return true;}
-    public void VerifyLogin (String username,String password){
-
-    }
-    public void NotifyPseudoChange(){}
+    
 }
