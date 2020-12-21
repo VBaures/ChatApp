6152a71f39fc5968446cd7820e66153f21e9f19f
VBaures <bauresvincent@outlook.com>
2020-12-21 15:19:00 +0100

    FileMessage

--- Code_POO/src/ChatApp/FileMessage.java
+++ Code_POO/src/ChatApp/FileMessage.java
@@ -8,13 +8,18 @@ import java.nio.file.Paths;
 
 public class FileMessage extends Message implements Serializable {
     protected byte[] content;
+    protected String fileName;
 
     public FileMessage (User recipient, User sender, String filePath) throws IOException {
         super(recipient,sender);
         Path path = Paths.get(filePath);
+        this.fileName = path.getFileName().toString();
         this.content= Files.readAllBytes(path);
     }
 
     public byte[] getContentFile (){return this.content;}
 
+    public String getFileName() {
+        return this.fileName;
+    }
 }
