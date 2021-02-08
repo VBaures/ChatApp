129be655254395f442d5f47334f317381720b030
AliciaCalmet <acalmet@etud.insa-toulouse.fr>
2021-02-08 20:02:25 +0100

    changement

--- Code_POO/src/ChatApp/PseudoPage.java
+++ Code_POO/src/ChatApp/PseudoPage.java
@@ -18,6 +18,8 @@ public class PseudoPage implements ActionListener {
     Agent agent;
     JTextField ppseudo;
     JFrame frame;
+    JButton bouton1;
+    JButton bouton2;
 
     //déclaration du constructeur de la classe
     public PseudoPage (Agent agent){
@@ -41,20 +43,25 @@ public class PseudoPage implements ActionListener {
 
         //gestion et création des composants
         JPanel panel1= new JPanel(new BorderLayout());
+        JPanel panel2= new JPanel(new BorderLayout());
         JLabel title= new JLabel("Veuillez rentrer un pseudo");
         JLabel pseudo = new JLabel("Pseudo ", SwingConstants.LEFT);
         ppseudo= new JTextField(SwingConstants.RIGHT);
-        JButton bouton= new JButton("VALIDER");
-        bouton.addActionListener(this );
+        bouton1= new JButton("Indoor User");
+        bouton1.addActionListener(this );
+        bouton2= new JButton("Outdoor User");
+        bouton2.addActionListener(this );
 
         //ajout composant panels
         panel1.add(title,BorderLayout.PAGE_START);
         panel1.add(pseudo,BorderLayout.LINE_START);
         panel1.add(ppseudo,BorderLayout.CENTER);
-        panel1.add(bouton, BorderLayout.PAGE_END);
+        panel2.add(bouton1, BorderLayout.PAGE_START);
+        panel2.add(bouton2,BorderLayout.PAGE_END);
 
         //ajout panels dans la fenetre
-        frame.getContentPane().add(panel1, BorderLayout.CENTER);
+        frame.getContentPane().add(panel1, BorderLayout.PAGE_START);
+        frame.getContentPane().add(panel2, BorderLayout.PAGE_END);
 
         //gestion fenêtre
         frame.pack();
@@ -63,6 +70,9 @@ public class PseudoPage implements ActionListener {
     /* gestion du bouton: on vérifie l'unicité du pseudonyme choisi par l'utilisateur et on en informe
     les autres utilisateurs de l'application */
     public void actionPerformed (ActionEvent e){
+        if (e.getSource()==bouton1){
+            
+        }
         String getvalue= ppseudo.getText();
         System.out.println("Message :"+getvalue);
         System.out.println(agent.getPseudoHandler().VerifyPseudo(getvalue));
